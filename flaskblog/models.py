d5847db7f4040a0b62adc5eb9ccb39367b250269
Glenn Lehman <glnn.lhmn@gmail.com>
2022-07-01 15:11:44 -0400

    completed - Full-Featured Web App Part 10
    Emails is not enabled - need to look at which smtp email to use.

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+# from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
+from itsdangerous import TimestampSigner, BadSignature, SignatureExpired
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 
@@ -15,6 +17,38 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    # def get_reset_token(self, expires_sec=1800):
+    #     s = Serializer(app.config['SECRET_KEY'], expires_sec)
+    #     return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    def get_reset_token(self):
+        s = TimestampSigner(app.config['SECRET_KEY'])
+        token = s.sign(str(self.id))
+        return token
+
+    # @staticmethod
+    # def verify_reset_token(token):
+    #     s = Serializer(app.config['SECRET_KEY'])
+    #     try:
+    #         user_id = s.loads(token)['user_id']
+    #     except:
+    #         return None
+    #     return User.query.get(user_id)
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = TimestampSigner(app.config['SECRET_KEY'])
+        try:
+            user_id = s.unsign(token, max_age=3600)
+        except SignatureExpired:
+            return None  # valid token, but expired
+        except BadSignature:
+            return None  # invalid token
+
+        # Get the user from the database
+        user = User.query.get(user_id)
+        return user
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
